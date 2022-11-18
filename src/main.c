ffae4fb4bf7be4b2224ea296d68a2e281b100e04
yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2022-11-18 18:51:42 +0100

    args parsing implemented

--- src/main.c
+++ src/main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <argp.h>
+#include "../include/args_parser.h"
 #include "../include/array_list.h"
 #include "../include/csv_parser.h"
 #include "../include/bin_builder.h"
 #include "../include/visualise.h"
 
+
 int main(int argc, char* argv[]){
-    FILE* fp = fopen("../les-arbres.csv", "r");
+    struct arguments arguments;
+    arguments.output_file = "-";
+    arguments.input_file = "-";
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    FILE* fp = fopen(arguments.input_file , "r");
     if(fp == NULL){ printf("file couldn't be read.\n"); exit(1); }
     
-    if(build_bin(fp, "coor.bin") == EXIT_FAILURE){ exit(1); }
+    if(build_bin(fp, arguments.output_file) == EXIT_FAILURE){ exit(1); }
 
-    FILE* fp_bin = fopen("coor.bin", "r");
+    FILE* fp_bin = fopen(arguments.output_file, "r");
     if(fp_bin == NULL){ printf("file couldn't be read.\n"); exit(1); }
 
     list_t* data_list = get_data_bin(fp_bin);
