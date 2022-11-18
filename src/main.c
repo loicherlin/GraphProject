62ac00cb813bb1823a6f27252175900492c623b5
yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2022-11-18 19:13:45 +0100

    added comments in main added a fix in size_column in csv_parser

--- src/main.c
+++ src/main.c
@@ -6,24 +6,30 @@
 #include "../include/bin_builder.h"
 #include "../include/visualise.h"
 
+void free_list(list_t* data_list){
+    for(size_t i = 0; i < list_size(data_list); i++)
+        free(list_get(data_list, i));
+    list_free(data_list);
+}
 
 int main(int argc, char* argv[]){
+    // Initiate arguments for args parsing
     struct arguments arguments;
     arguments.output_file = "-";
     arguments.input_file = "-";
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
-
+    // Open CSV file
     FILE* fp = open_file(arguments.input_file);
-
+    // Build bin file based on fp
     int result = build_bin(fp, arguments.output_file);
     if(result == EXIT_FAILURE){ fclose(fp); exit(1); }
+    // Open bin file to read it
     FILE* fp_bin = open_file(arguments.output_file);
-
     list_t* data_list = get_data_bin(fp_bin);
+    // Show a visualization of data from the binary file
     show_data(1300, 900, data_list);
-    for(size_t i = 0; i < list_size(data_list); i++)
-        free(list_get(data_list, i));
-    list_free(data_list);
+    // Free content of the list and the list itself
+    free_list(data_list);
     fclose(fp_bin);
     return 0;
 }
