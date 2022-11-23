700eb7bbbff92cc1a935150f0ba629088705b2e7
yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2022-11-23 10:11:51 +0100

    edge_t struct added

--- include/delauney.h
+++ include/delauney.h
@@ -8,7 +8,12 @@ typedef struct triangle
     node_t s1;
     node_t s2;
     node_t s3;
-}triangle;
+} triangle;
+
+typedef struct edge{
+    node_t org;
+    node_t dest;
+} edge_t;
 
 triangle create_triangle(node_t a, node_t b, node_t c);
 triangle create_super_triangle(list_t* nodes);
