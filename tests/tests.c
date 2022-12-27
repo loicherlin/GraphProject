be7ebf29c0a724329378fc2e0ae3049fd9b8d808
yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2022-12-27 12:23:40 +0100

    tests folder added

---
+++ tests/tests.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/args_parser.h"
+#include "../include/array_list.h"
+#include "../include/bin_builder.h"
+#include "../include/cprintf.h"
+#include "../include/csv_parser.h"
+#include "../include/data_t.h"
+#include "../include/delaunay.h"
+#include "../include/graph.h"
+#include "../include/handler.h"
+#include "../include/min_heap.h"
+#include "../include/triangle.h"
+#include "../include/visualise.h"
+
+#include "tps_unit_test.h"
+
+int main(void)
+{
+    printf("eok");
+    return 0;
+}
