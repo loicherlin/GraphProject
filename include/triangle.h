a9c43aebab1f8a930bb237104a83b7457fc4f49a
Herlin Loïc <loic.herlin@etu.unistra.fr>
2023-01-09 13:55:45 +0100

    doc

--- include/triangle.h
+++ include/triangle.h
@@ -13,10 +13,45 @@ typedef struct edge{
     data_t* dest;
 } edge_t;
 
+/***
+ * @brief Create a triangle object with 3 points
+ * 
+ * @param a 
+ * @param b 
+ * @param c 
+ * @return triangle_t 
+*/
 triangle_t create_triangle(data_t* a, data_t* b, data_t* c);
 
+/***
+ * @brief Compare a edge with a triangle if they share a same edge
+ * 
+ * @param bad_t_a 
+ * @param bad_t_b
+ * @param bad_t_c
+ * @param current
+ * @param EPSILON
+ * @return int return 1 if they share a same edge else 0
+*/
 int edge_shared(edge_t bad_t_a, edge_t bad_t_b, edge_t bad_t_c, edge_t current, double ESPILON);
+
+/***
+ * @brief Compare two triangles if they share a same point
+ * 
+ * @param a
+ * @param b
+ * @param EPSILON
+ * @return int return 1 if they share a same point else 0
+*/
 
 int compare_triangle_point(triangle_t a, triangle_t b, double EPSILON);
 
+/***
+ * @brief Compare two triangles if they are the same triangle return 1 else 0
+ * 
+ * @param a triangle
+ * @param b triangle
+ * @param EPSILON double
+ * @return int return 1 if they are the same triangle else 0
+*/
 int compare_triangle(triangle_t a, triangle_t b, double EPSILON);
